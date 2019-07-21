77f8c120854f1c0bc0d424fa72e1a0edcc9758c0
sksdk34 <gim34@naver.com>
2019-07-21 15:56:16 +0900

    file upload

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,7 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var mysql = require('mysql');
+var mysql = require('mysql'); //mysql 모듈
+var multer = require('multer'); //파일 업로드 모듈
+var upload = multer({dest: 'uploads/'}) //입력한 파일이 uploads/ 폴더에 저장
 var router = express.Router();
 
 var conn = mysql.createConnection({
@@ -61,18 +63,23 @@ router.get('/posts/create', function(req, res){
 })
 
 //게시글 작성 처리
-router.post('/posts/create', function(req, res){
+router.post('/posts/create', upload.single('uploadFile'), function(req, res){
   var title = req.body.title;
   var content = req.body.content;
   var writer = req.cookies.login;
+  var originalname = req.file.originalname;
+  var filename = req.file.filename;
 
-  var query = 'insert into posts (title, content, writer, date) values ("' + title + '" , "' + content + '" , "' + writer + '" ,now())';
+  var query = 'insert into posts (title, content, writer, date, filename, path) values ("' + title + '" , "' + content + '" , "' + writer + '" ,now(), "' + originalname + '", "' + filename + '")';
   conn.query(query, function(err, result){
     if(err) throw err;
 
     console.log("title : " + title);
     console.log("body : " + content);
     console.log("id : " + writer);
+    console.log("orginalname : " + originalname);
+    console.log("filename : " + filename);
+    console.log(req.file);
 
     console.log("Insert Success");
 
@@ -105,6 +112,26 @@ router.get('/posts/detail/:no', function(req, res){
       res.end(data);
     })
   }
+})
+
+//파일 다운로드
+router.get('/posts/detail/:no/download', function(req, res){
+
+  var query = 'select * from posts where no = ?';
+  conn.query(query, req.params.no, function(err, result){
+
+    var originalname = result[0].filename;
+    var path = __dirname + '/../uploads/' + result[0].path;
+    console.log(originalname);
+    console.log(path);
+
+    res.setHeader("Content-Disposition", "attachment;filename=" + encodeURI(originalname));
+    res.setHeader("Content-Type","binary/octet-stream");
+    // filePath에 있는 파일 스트림 객체를 얻어온다.(바이트 알갱이를 읽어옵니다.)
+    var fileStream = fs.createReadStream(path);
+    // 다운로드 한다.(res 객체에 바이트알갱이를 전송한다)
+    fileStream.pipe(res);
+  })
 })
 
 //게시글 삭제
@@ -171,7 +198,20 @@ router.get('/posts/detail/:no/update', function(req, res){
   var check = req.cookies.login;
 
   if(check != null){
-    res.render('update');
+
+    var query = 'select * from posts where no = ?';
+    conn.query(query, req.params.no, function(err, result){
+      if(err) throw err
+
+      var data = new Object;
+      data = result[0];
+
+      var jsonData = JSON.stringify(data);
+      console.log("UPDATE" + req.params.no + "PAGE");
+      console.log(jsonData);
+
+      res.render('update', {result:jsonData});
+    })
   }else{
     fs.readFile(__dirname + '/views/check.html', function(err, data){
       res.writeHead(200, {'Content-Type':'text/html'});
@@ -225,6 +265,19 @@ router.get('/posts/logout', function(req, res){
       res.end(data);
     })
   }
+})
+
+router.get('/test', function(req, res){
+
+  res.render('test');
+})
+
+router.post('/test', upload.single('userfile'), function(req,res){
+
+  console.log(req.file);
+  console.log(req.file.originalname);
+
+  res.send(req.file.originalname);
 })
 
 exports.router = router;
